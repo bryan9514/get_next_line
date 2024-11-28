461a24d88241fd98d5437b68504ca76b1bb75c51
Bryan <btjosueturcios@gmail.com>
2024-11-28 06:18:37 +0100

    28-novi-home

--- get_next_line.h
+++ get_next_line.h
@@ -6,8 +6,15 @@
 /*   By: brturcio <brturcio@student.42angouleme.fr  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 18:45:00 by brturcio          #+#    #+#             */
-/*   Updated: 2024/11/21 18:45:02 by brturcio         ###   ########.fr       */
+/*   Updated: 2024/11/27 18:41:06 by brturcio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE
+#define GET_NEXT_LINE
 
+#include <fcntl.h>
+#incldue <stdio.h>
+
+
+#endif
--- main.c
+++ main.c
@@ -1,13 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: brturcio <brturcio@student.42angouleme.fr  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/21 18:44:30 by brturcio          #+#    #+#             */
-/*   Updated: 2024/11/25 17:13:49 by brturcio         ###   ########.fr       */
+/*   Created: 2024/11/25 18:46:22 by brturcio          #+#    #+#             */
+/*   Updated: 2024/11/27 21:13:17 by brturcio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+int	main(void)
+{
+	int fd = open (/home/bt24/42 Ecole/get_next_line/file.txt,int O_RDONLY);
+	printf ("%s", get_next_line(fd));
+	free(fd);
+	return (0);
+}
